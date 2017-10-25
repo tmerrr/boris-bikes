6a9d29e8a089437afd274f8b79a126c4bbc183d0
Tom Moir <trmoir9@gmail.com>
2017-10-25 11:41:04 +0100

    started step 12

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -3,14 +3,19 @@ class DockingStation
   attr_reader :num_bikes
 
   def initialize
-    @num_bikes = 6
+    @num_bikes = 0
   end
 
   def release_bike
+    fail 'No bikes available' unless any_bikes?
     Bike.new
   end
 
   def return_bike
     @num_bikes += 1
+  end
+
+  def any_bikes?
+    @num_bikes > 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,16 @@
-require 'Docking_station.rb'
+require 'docking_station.rb'
 
 describe DockingStation do
-    it {is_expected.to be_a(DockingStation)}
+    it { is_expected.to be_a(DockingStation) }
 
     it { is_expected.to respond_to(:release_bike) }
+
     it 'creates a bike' do
       expect(subject.release_bike).to be_instance_of(Bike)
     end
 
     it { is_expected.to respond_to(:return_bike) }
+
     it { is_expected.to respond_to(:num_bikes) }
     context 'when return_bike is called' do
       it 'adds 1 to num_bikes' do
@@ -17,5 +19,19 @@ describe DockingStation do
         expect(subject.num_bikes).to eq (bikes_before + 1)
       end
     end
+
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
+
+    it { expect(subject.any_bikes?).to eq subject.num_bikes > 0 ? true : false }
+
+    # describe '#any_bikes?' do
+    #   it 'returns NoMethodError before it\'s been created' do
+    #     expect { subject.any_bikes }.to raise_error 'Method .any_bikes? not created'
+    #   end
+    # end
 
 end
